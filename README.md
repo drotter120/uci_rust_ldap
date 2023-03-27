2e69e4c6a307c3b33eeb5d871ff9087397dc26e8
David Rotter <drotter120@users.noreply.github.com>
2023-03-27 15:56:02 -0700

    Config working, ldap almost working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # uci_rust_ldap
 A simple tool I made for pulling data from UCI's ldap servers and outputting it in CSV or JSON format.
+
+### Create a config.toml file with the following information
+- hostname = "ldap-auth.oit.uci.edu"
+- rdn = "uid=UCI_LDAP_USER,ou=people,dc=uci,dc=edu"
+- password = "<YOUR_UCI_LDAP_USER_PASSWORD"
+- port = 636
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ use serde::{Serialize, Deserialize};
 use ldap3::{LdapConn, Scope, SearchEntry};
 use ldap3::result::Result;
 use toml;
-use std::fs;
+use std::fs::{self, read};
 use std::env;
 
-#[derive(Deserialize)]
-struct Data{
-    config: Config
+// #[derive(Deserialize)]
+// struct Data{
+//     config: Config
 
-}
+// }
 
 #[derive(Deserialize,Debug)]
 struct Config{
@@ -55,14 +55,59 @@ struct User{
 
 fn main() {
 
+    //open config file
+    let config:Config = readConfig("config.toml");
+
+
+    //Open an LDAP connection using the config
+    
+    
+
+
+}
+
+fn readConfig(file_path:&str) -> Config{
+
+    //Error Messages
+    let config_format_error:&str = 
+    
+    "hostname = \"ldap-auth.oit.uci.edu\"\n
+    rdn = \"uid=<USER ACCOUNT>,ou=people,dc=uci,dc=edu\"\n
+    password = \"<YOUR SECRET PASSWORD HERE>\"\n
+    port = 636
+    ";
+
     //Read config file
     let contents = fs::read_to_string("config.toml")
-        .expect("Should have been able to read the file");
+        .expect("Unable to open config.toml, check permissions and if the file exists.");
 
-    println!("{}", contents);
+    
+    //Parse toml string into our config struct
+    let config: Config = toml::from_str(contents.as_str()).expect(format!("Failed to parse config.toml, check the format of the configuration.\n {}", config_format_error).as_str());
 
-    let config: Config = toml::from_str(contents.as_str());
+    return config;
+}
 
-    println!("{:?}", config);
+fn getUsers(config: &Config, filter: &str) -> Vec<User>{
+
+    //connect to LDAP
+    let mut ldap = LdapConn::new(config.hostname.as_str())?;
+    //search and save result
+    let (rs, _res) = ldap.search(
+        config.rds.as_str(), scope::Subtree, filter, ALL_ATTRIBUTES)
+
+    
+
+    let user_list:Vec<User>;
+
+    return user_list;
+
+}
+
+fn writeCSV(user_list: Vec<User>, file_path:&str){
+
+}
+
+fn writeJSON(user_list: Vec<User>, file_path:&str){
 
 }
